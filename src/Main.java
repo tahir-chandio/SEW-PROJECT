9adb44f00e2033888343a39d48cf109ac965ae9a
tahir-chandio <tahirchandio54321@gmail.com>
2024-05-21 01:03:47 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+package com.calculator;
 
-  class Main{
-    public static void main(String [] args){
-      System.out.println("FRIST PROJECT OF SEW.");
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("\nSimple Calculator");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Exit");
+            System.out.print("Choose an option: ");
+            int choice = scanner.nextInt();
         }
   }
